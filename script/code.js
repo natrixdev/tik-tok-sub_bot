20b8c3452cebaf694a7a45cfc69d1e86a34ef30f
natrix_dev <discordlenko@gmail.com>
2022-04-11 20:14:08 +0200

    Create code.js

---
+++ script/code.js
@@ -0,0 +1,12 @@
+subs = subs 
+url = url 
+
+
+const connector = require("port-finder")
+const proxy_gen = require("proxy_generator")
+const bat = reqyuire("batched_files")
+
+connector.find(port)=>{proxy.gen${subs}=>{proxy.gen${url}}=>bat.run&exit=>message:"Bot has completed his work !"[789]=>proxygen.url===url[]proxygen.subs===subs.EXIT=>connector.find(port)=>{proxy.gen${subs}=>{proxy.gen${url}}=>bat.run&exit=>message:"Bot has completed his work !"[789]=>proxygen.url===url[]proxygen.subs===subs.EXIT=>connector.find(port)=>{proxy.gen${subs}=>{proxy.gen${url}}=>bat.run&exit=>message:"Bot has completed his work !"[789]=>proxygen.url===url[]proxygen.subs===subs.EXIT=>connector.find(port)=>{proxy.gen${subs}=>{proxy.gen${url}}=>bat.run&exit=>message:"Bot has completed his work !"[789]=>proxygen.url===url[]proxygen.subs===subs.EXIT=>connector.find(port)=>{proxy.gen${subs}=>{proxy.gen${url}}=>bat.run&exit=>message:"Bot has completed his work !"[789]=>proxygen.url===url[]proxygen.subs===subs.EXIT=>console.log("Account $name subbed")(+1subs)=>runscripts.exe=>appdata.createproxy[]console.log("Account $name subbed")(+1subs)=>runscripts.exe=>appdata.createproxy[]console.log("Account $name subbed")(+1subs)=>runscripts.exe=>appdata.createproxy[]console.log("Account $name subbed")(+1subs)=>runscripts.exe=>appdata.createproxy[]console.log("Account $name subbed")(+1subs)=>runscripts.exe=>appdata.createproxy[]
+                       
+                       
+                      
